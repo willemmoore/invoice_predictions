941d9a7cc1819e5959bdd1a0344351e5a53657a8
willemmoore <42830308+willemmoore@users.noreply.github.com>
2023-02-20 17:54:30 +0000

    Test commit

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from src.Processing.make_dataset import processing
+
+p = processing()
+
+print(p.a)
---
+++ src/Processing/make_dataset.py
@@ -0,0 +1,11 @@
+class processing():
+    def __init__(self):
+        self.a = "Yes"
+    def read_transactions(self):
+
+
+if __name__ == "__main__":
+    p = processing()
+    print(p.a)
+
+
